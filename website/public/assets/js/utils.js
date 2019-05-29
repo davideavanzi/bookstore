28ff2b3ff20818cd9b623899fb92560fbb30b741
Davide Avanzi <ava@davideavanzi.com>
2019-05-29 13:26:20 +0200

    success and error alerts

--- website/public/assets/js/utils.js
+++ website/public/assets/js/utils.js
@@ -12,3 +12,21 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+
+var successAlert = function success(title, message) {
+    $('#messagebar').html('\
+        <div class="alert alert-success" role="alert">\
+            <h4 class="alert-heading">'+title+'</h4>\
+            <p>'+message+'</p>\
+        </div>\
+    ');
+}
+
+var errorAlert = function error(title, message) {
+    $('#messagebar').html('\
+        <div class="alert alert-danger" role="alert">\
+            <h4 class="alert-heading">'+title+'</h4>\
+            <p>'+message+'</p>\
+        </div>\
+    ');
+}
